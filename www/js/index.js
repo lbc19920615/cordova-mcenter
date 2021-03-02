e4341862545a6dfd5b25c7f5f0d3059e31225658
lbc19920615 <1585638808@qq.com>
2021-03-02 17:12:48 +0800

    测试电量

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,12 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+
+    window.addEventListener("batterystatus", onBatteryStatus, false);
+
+    function onBatteryStatus(status) {
+        console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+        document.getElementById('battery').innerHTML = status.level + ''
+    }
 }
